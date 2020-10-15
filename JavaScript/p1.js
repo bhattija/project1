b4e829dd6526c891bd902e7eb456815d1821f233
Jared Bhatti <bhattija@dukes.jmu.edu>
2020-10-15 18:16:10 -0400

    Added Files to specified folders

---
+++ JavaScript/p1.js
@@ -0,0 +1,21 @@
+document.getElementById("home").onclick = function () {
+    location.href = "index.html";
+};
+document.getElementById("About").onclick = function () {
+    location.href = "about.html";
+};
+document.getElementById("Services").onclick = function () {
+    location.href = "services.html";
+};
+document.getElementById("Gallary").onclick = function () {
+    location.href = "gallary.html";
+};
+document.getElementById("Contact").onclick = function () {
+    location.href = "form.html";
+};
+document.getElementById("Serv").onclick = function () {
+    location.href = "services.html";
+};
+document.getElementById("Abut").onclick = function () {
+    location.href = "about.html";
+};
