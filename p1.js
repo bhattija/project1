7c341468be989c19993a03bdcf61ac64384c6778
Jared Bhatti <bhattija@dukes.jmu.edu>
2020-10-15 16:36:53 -0400

    Final Touches

--- p1.js
+++ p1.js
@@ -1,21 +1,21 @@
-document.getElementById( "home").onclick = function(){
+document.getElementById("home").onclick = function () {
     location.href = "index.html";
 };
-document.getElementById( "About").onclick = function(){
+document.getElementById("About").onclick = function () {
     location.href = "about.html";
 };
-document.getElementById( "Services").onclick = function(){
+document.getElementById("Services").onclick = function () {
     location.href = "services.html";
 };
-document.getElementById( "Gallary").onclick = function(){
+document.getElementById("Gallary").onclick = function () {
     location.href = "gallary.html";
 };
-document.getElementById( "Contact").onclick = function(){
+document.getElementById("Contact").onclick = function () {
     location.href = "form.html";
 };
-document.getElementById( "Serv").onclick = function(){
+document.getElementById("Serv").onclick = function () {
     location.href = "services.html";
 };
-document.getElementById( "Abut").onclick = function(){
+document.getElementById("Abut").onclick = function () {
     location.href = "about.html";
 };
